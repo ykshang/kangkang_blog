e4cd92224c28ced032fe321ae5d611b1f8c86793
kangkang shang <535634238@qq.com>
2020-12-27 00:42:53 +0800

    添加两个插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,15 +6,23 @@ module.exports = {
   // markdown: {
   //   lineNumbers: true // 显示行号
   // },
+  plugins: [
+    'reading-progress',
+    [
+      "vuepress-plugin-gotop-plus",
+      {
+        threshold: 10 // 10%
+      }
+    ]
+  ],
   themeConfig: {
     displayAllHeaders: true,
     logo: '/assets/img/logo.gif', // 导航栏的logo
     nav: nav_config, // 导航栏右侧的链接配置
     sidebar: sidebar_config, // 侧边栏配置
-    lastUpdated: 'Last Updated', // 文档的最后更新时间 可选值: string | boolean
+    lastUpdated: true, // 文档的最后更新时间 可选值: string | boolean
     repo: 'ykshang/kangkang_blog', // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
     repoLabel: '查看源码',// 自定义仓库链接文字。
-    
     // ? 以下为可选的编辑链接选项
     docsDir: 'docs',  // 假如文档不是放在仓库的根目录下：
     docsBranch: 'master', // 假如文档放在一个特定的分支下：
