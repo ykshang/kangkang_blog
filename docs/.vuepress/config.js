de1fcaa18b9ca94b7b470bd1ce263b7bd2482377
kangkang shang <535634238@qq.com>
2020-12-28 23:32:18 +0800

    修改配置显示行号

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,9 +5,9 @@ module.exports = {
     ['link', { rel: 'shortcut icon', type: "image/x-icon", href: "/logo.png" }] // 网页favicon
   ],
   description: '记录学习成长', // 不知道干嘛的
-  // markdown: {
-  //   lineNumbers: true // 显示行号
-  // },
+  markdown: {
+    lineNumbers: true // 显示行号
+  },
   plugins: require("./config/plugins/index"),
   themeConfig: require("./config/theme_config/index") // 主题配置
 }
