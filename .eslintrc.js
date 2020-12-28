953dfaf3a082fc58249abb3035f17b90c9f2b894
kangkang shang <535634238@qq.com>
2020-12-28 23:02:58 +0800

    引入eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
