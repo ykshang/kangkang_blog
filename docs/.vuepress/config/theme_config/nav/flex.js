68bfbff49332dc8308c1ffde1d3f407c448faa3d
kangkang shang <535634238@qq.com>
2020-12-30 12:24:16 +0800

    flex布局结构搭建

--- docs/.vuepress/config/theme_config/nav/flex.js
+++ docs/.vuepress/config/theme_config/nav/flex.js
@@ -1,4 +1,46 @@
 module.exports = {
   text: 'Flex弹性布局',
-  link: '/flex/index'
+  items: [{
+    text: '简介',
+    link: '/flex/index'
+  }, {
+    text: '相关概念',
+    link: '/flex/introduction'
+  }, {
+    text: '初始化计算',
+    link: '/flex/init'
+  }, {
+    text: '弹性元素',
+    link: '/flex/item/item'
+  }, {
+    text: '弹性容器',
+    link: '/flex/container/container'
+  }, {
+    text: '外在影响因素',
+    items: [{
+      text: 'min-content/max-content',
+      link: '/flex/other/css1'
+    }, {
+      text: 'position',
+      link: '/flex/other/css2'
+    }, {
+      text: 'direction',
+      link: '/flex/other/css3'
+    }, {
+      text: 'margin',
+      link: '/flex/other/css4'
+    }, {
+      text: 'min-width/overflow',
+      link: '/flex/other/css5'
+    }]
+  }, {
+    text: '应用实例',
+    items: [{
+      text: '表格hover组件',
+      link: '/flex/example/example1'
+    }, {
+      text: '表格hover组件2',
+      link: '/flex/example/example2'
+    }]
+  }]
 }
--- docs/.vuepress/config/theme_config/sidebar/flex.js
+++ docs/.vuepress/config/theme_config/sidebar/flex.js
@@ -1,4 +1,46 @@
 module.exports = {
   title: 'Flex弹性布局',
-  path: '/flex/'
+  children: [{
+    title: '简介',
+    path: '/flex/'
+  }, {
+    title: '相关概念',
+    path: '/flex/introduction'
+  }, {
+    title: '初始化计算',
+    path: '/flex/init'
+  }, {
+    title: '弹性元素',
+    path: '/flex/item/item'
+  }, {
+    title: '弹性容器',
+    path: '/flex/container/container'
+  }, {
+    title: '外在影响因素',
+    children: [{
+      title: 'min-content/max-content',
+      path: '/flex/other/css1'
+    }, {
+      title: 'position',
+      path: '/flex/other/css2'
+    }, {
+      title: 'direction',
+      path: '/flex/other/css3'
+    }, {
+      title: 'margin',
+      path: '/flex/other/css4'
+    }, {
+      title: 'min-width/overflow',
+      path: '/flex/other/css5'
+    }]
+  }, {
+    title: '应用实例',
+    children: [{
+      title: '表格hover组件',
+      path: '/flex/example/example1'
+    }, {
+      title: '表格hover组件2',
+      path: '/flex/example/example2'
+    }]
+  }]
 }
