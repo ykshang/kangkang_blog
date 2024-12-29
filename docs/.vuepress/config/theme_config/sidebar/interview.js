6731ea7f3d2b6e0b1413f44098b704ba60ee0703
kangkang shang <535634238@qq.com>
2024-12-29 18:09:04 +0800

    增加二级分类

--- docs/.vuepress/config/theme_config/sidebar/interview.js
+++ docs/.vuepress/config/theme_config/sidebar/interview.js
@@ -1,10 +1,11 @@
 module.exports = {
   title: '常见面试题',
   children: [{
-    title: '深拷贝和浅拷贝',
-    path: '/interview/1'
-  }, {
-    title: '香菇蒸滑鸡',
-    path: '/cookbook/1'
+    title: 'JS基础',
+    path: '/interview/basic/index',
+    children: [{
+      title: '深拷贝和浅拷贝',
+      path: '/interview/basic/1'
+    }]
   }]
 }
